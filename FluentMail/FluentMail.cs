661fe68347b336d24d091b756a059fdb58c486bf
Nelson Esmarlin Rosario Rodriguez <esmarlin9@gmail.com>
2024-05-29 18:16:05 -0400

    enhancement: Replace string by string builder

--- FluentMail/FluentMail.cs
+++ FluentMail/FluentMail.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics.Metrics;
 using System.Linq;
 using System.Reflection.Metadata;
+using System.Text;
 using System.Threading.Tasks;
 using FluentMail.Elements;
 
@@ -14,42 +15,44 @@ namespace FluentMail
         private string headContent;
         private string bodyContent;
 
-        public FluentMail Html(Action<HtmlConfig> htmlConfig)
+        public FluentMail Html(Action<HtmlConfig> html)
         {
             var config = new HtmlConfig();
-            htmlConfig(config);
+            html(config);
             this.lang = config.Languague;
             return this;
         }
 
-        public FluentMail Head(Action<HeadBuilder> headConfig)
+        public FluentMail Head(Action<HeadBuilder> head)
         {
             var headBuilder = new HeadBuilder();
-            headConfig(headBuilder);
+            head(headBuilder);
             this.headContent = headBuilder.Build();
             return this;
         }
 
-        public FluentMail Body(Action<BodyBuilder> bodyConfig)
+        public FluentMail Body(Action<BodyBuilder> body)
         {
             var bodyBuilder = new BodyBuilder();
-            bodyConfig(bodyBuilder);
+            body(bodyBuilder);
             this.bodyContent = bodyBuilder.Build();
             return this;
         }
 
         public string Build()
         {
-            return $@"
-                <!DOCTYPE html>
-                <html lang=""{lang}"">
-                <head>
-                    {headContent}
-                </head>
-                <body>
-                    {bodyContent}
-                </body>
-                </html>";
+            var builder = new StringBuilder();
+            builder.AppendFormat(@"
+            <!DOCTYPE html>
+            <html lang=""{0}"">
+            <head>
+                {1}
+            </head>
+            <body>
+                {2}
+            </body>
+            </html>", lang, headContent, bodyContent);
+            return builder.ToString();
         }
     }
 
@@ -77,171 +80,102 @@ namespace FluentMail
 
         public HeadBuilder Meta(string name, string content)
         {
-            this.metas.Add($@"<meta name=""{name}"" content=""{content}"">");
+            metas.Add($@"<meta name=""{name}"" content=""{content}"">");
             return this;
         }
 
         public HeadBuilder Meta(string charset)
         {
-            this.metas.Add($@"<meta charset=""{charset}"">");
+            metas.Add($@"<meta charset=""{charset}"">");
             return this;
         }
 
         public string Build()
         {
             var metaTags = string.Join("\n    ", metas);
-            return $@"<title>{title}</title>{metaTags}";
+            return $"<title>{title}</title>\n    {metaTags}";
         }
     }
 
     public class BodyBuilder
     {
-        private List<string> rows = new List<string>();
+        private readonly StringBuilder rowsBuilder = new StringBuilder();
 
-        public BodyBuilder Row(Action<RowBuilder> rowConfig)
+        public BodyBuilder Row(Action<RowBuilder> row)
         {
             var rowBuilder = new RowBuilder();
-            rowConfig(rowBuilder);
-            rows.Add(rowBuilder.Build());
+            row(rowBuilder);
+            rowsBuilder.Append(rowBuilder.Build());
             return this;
         }
 
         public string Build()
         {
-            return string.Join("\n", rows);
+            return rowsBuilder.ToString();
         }
     }
 
-
     public class RowBuilder
     {
-        private string backgroundColor;
-        private string style;
-        private List<ColumnBuilder> columns = new List<ColumnBuilder>();
+        private readonly StringBuilder columnsBuilder = new StringBuilder();
 
-        public RowBuilder BackgroundColor(string color)
-        {
-            this.backgroundColor = color;
-            return this;
-        }
-
-        public RowBuilder Style(string style)
-        {
-            this.style = style;
-            return this;
-        }
-
-        public RowBuilder AddStyle(string style)
-        {
-            this.style += style;
-            return this;
-        }
-
-        public RowBuilder Column(Action<ColumnBuilder> columnConfig)
+        public RowBuilder Column(Action<ColumnBuilder> column)
         {
             var columnBuilder = new ColumnBuilder();
-            columnConfig(columnBuilder);
-            columns.Add(columnBuilder);
+            column(columnBuilder);
+            columnsBuilder.Append(columnBuilder.Build());
             return this;
         }
 
         public string Build()
         {
-            int specifiedWidthColumns = columns.Count(col => !string.IsNullOrEmpty(col.GetWidth()));
-            int unspecifiedWidthColumns = columns.Count - specifiedWidthColumns;
-
-            string autoWidth = unspecifiedWidthColumns > 0 ? $"{100 / unspecifiedWidthColumns}%" : "100%";
-
-            var columnHtml = columns.Select(col =>
-            {
-                if (string.IsNullOrEmpty(col.GetWidth()))
-                {
-                    col.Width(autoWidth);
-                }
-                return col.Build();
-            });
-
-            var rowStyle = !string.IsNullOrEmpty(backgroundColor) ? $"background-color: {backgroundColor};" : "";
-                        return $@"
-            <table width=""100%"" cellpadding=""0"" cellspacing=""0"" border=""0"" style=""table-layout: fixed; border-collapse: collapse;"">
-                <tr style=""{rowStyle} {style}"">
-                    {string.Join("\n", columnHtml)}
-                </tr>
-            </table>";
-            }
+            return $"<tr>{columnsBuilder}</tr>";
         }
+    }
+
     public class ColumnBuilder
     {
-        private string backgroundColor;
-        private string style;
-        private string content = "";
-        private string width;
-        private List<string> subRows = new List<string>();
-
-        public ColumnBuilder BackgroundColor(string color)
-        {
-            this.backgroundColor = color;
-            return this;
-        }
-
-        public ColumnBuilder Style(string style)
-        {
-            this.style = style;
-            return this;
-        }
+        private readonly StringBuilder contentBuilder = new StringBuilder();
 
         public ColumnBuilder Image(string url, string alt = "", string style = "")
         {
-            content += $@"<img src=""{url}"", alt=""{alt}"" style=""{style}""/>";
+            contentBuilder.AppendFormat(@"<img src=""{0}"" alt=""{1}"" style=""{2}""/>", url, alt, style);
             return this;
-        }
-
-        public ColumnBuilder Width(string width)
-        {
-            this.width = width;
-            return this;
-        }
-
-        public string GetWidth()
-        {
-            return this.width;
         }
 
         public ColumnBuilder H1(string text, string style = "")
         {
-            content += $@"<h1 style=""{style}"">{text}</h1>";
+            contentBuilder.AppendFormat(@"<h1 style=""{0}"">{1}</h1>", style, text);
             return this;
         }
 
         public ColumnBuilder Paragraph(string text, string style = "")
         {
-            content += $@"<p style=""{style}"">{text}</p>";
+            contentBuilder.AppendFormat(@"<p style=""{0}"">{1}</p>", style, text);
             return this;
         }
 
         public ColumnBuilder Button(string text, string url, string style = "")
         {
-            content += $@"<a href=""{url}"" style=""{style}"">{text}</a>";
-            return this;
-        }
-
-        public ColumnBuilder Row(Action<RowBuilder> rowConfig)
-        {
-            var rowBuilder = new RowBuilder();
-            rowConfig(rowBuilder);
-            subRows.Add(rowBuilder.Build());
+            contentBuilder.AppendFormat(@"<a href=""{0}"" style=""{1}"">{2}</a>", url, style, text);
             return this;
         }
 
         public string Build()
         {
-            var widthStyle = !string.IsNullOrEmpty(width) ? $"width: {width};" : "";
-            var subRowsContent = string.Join("\n", subRows);
-            return $@"
-<td style=""background-color: {backgroundColor}; vertical-align: top; {widthStyle} {style}"">
-    {content}
-    {subRowsContent}
-</td>";
+            string backgroundColor = "white";
+            string widthStyle = "width:100%";
+            string style = "";
+            string subRowsContent = "";
+
+            var builder = new StringBuilder();
+            builder.AppendFormat(@"
+                <td style=""background-color: {0}; vertical-align: top; {1} {2}"">
+                    {3}
+                    {4}
+                </td>", backgroundColor, widthStyle, style, contentBuilder.ToString(), subRowsContent);
+
+            return builder.ToString();
         }
     }
 
